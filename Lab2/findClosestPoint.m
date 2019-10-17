971527eca776cf14516db591a9184c7169fce5e6
Stephanie <steptang@seas.upenn.edu>
2019-10-17 19:11:05 -0400

    finished without bfs

--- Lab2/findClosestPoint.m
+++ Lab2/findClosestPoint.m
@@ -2,7 +2,7 @@ function [qClosest] = findClosestPoint(qSample, nodes)
     k = keys(nodes);
     closestDistance = 1000;
 	for i = 1:length(nodes)
-        currDistance = distance(qSample, nodes(k{i}));
+        currDistance = distance(qSample, nodes(k{i}).q);
         if currDistance < closestDistance
             closestDistance = currDistance;
             qClosest = k{i};
--- Lab2/rrt.m
+++ Lab2/rrt.m
@@ -25,13 +25,9 @@ numSteps = 10;
 % initialize variables
 startNodes = containers.Map;
 goalNodes = containers.Map;
-startGraph = digraph;
-goalGraph = digraph;
 
-startGraph = startGraph.addnode('start');
-goalGraph = goalGraph.addnode('goal');
-startNodes('start') = start(1:4);
-goalNodes('goal') = goal(1:4);
+startNodes('start') = struct('q', start(1:4), 'lastNode', -1);
+goalNodes('goal') = struct('q', goal(1:4), 'lastNode', -1);
 
 found = false;
 connectionPoint = 0;
@@ -44,23 +40,19 @@ for i = 1:numIter
     
     % find closest point in start and goal trees
     [qClosestStart] = findClosestPoint(qSample, startNodes);
-    [qClosestgoal] = findClosestPoint(qSample, goalNodes);
+    [qClosestGoal] = findClosestPoint(qSample, goalNodes);
     
     % discretize these two paths and check if points along path collide
-    isStartPathCollided = isPathCollided(qSample, startNodes(qClosestStart), map, robot, numSteps);
-    isGoalPathCollided = isPathCollided(qSample, goalNodes(qClosestgoal), map, robot, numSteps);
+    isStartPathCollided = isPathCollided(qSample, startNodes(qClosestStart).q, map, robot, numSteps);
+    isGoalPathCollided = isPathCollided(qSample, goalNodes(qClosestGoal).q, map, robot, numSteps);
     
     % add nodes to tree if not collided
     nodeIndex = int2str(i);
     if ~isStartPathCollided
-        startGraph = startGraph.addnode(nodeIndex);
-        startNodes(nodeIndex) = qSample;
-        startGraph = addedge(startGraph, qClosestStart, nodeIndex);
+        startNodes(nodeIndex) = struct('q', qSample, 'lastNode', qClosestStart);
     end
     if ~isGoalPathCollided
-        goalGraph = goalGraph.addnode(nodeIndex);
-        goalNodes(nodeIndex) = qSample;
-        goalGraph = addedge(goalGraph, qClosestgoal, nodeIndex);
+        goalNodes(nodeIndex) = struct('q', qSample, 'lastNode', qClosestGoal);
     end 
     if ~isStartPathCollided && ~isGoalPathCollided
     	found = true;
@@ -70,21 +62,21 @@ for i = 1:numIter
 end 
 
 if found
-    % find shortest path in the graph
-    startPath = shortestpath(startGraph,'start',connectionPoint);
-    goalPath = shortestpath(goalGraph,'goal',connectionPoint);
-    goalPath = flip(goalPath);
-    
-    % create path matrix
-    startTreeSize = size(startPath, 2);
-    goalTreeSize = size(goalPath, 2);
-    path = zeros(startTreeSize + goalTreeSize - 1, 6);
-    for i = 1: startTreeSize - 1
-        path(i, :) = [startNodes(startPath{i}) start(5) start(6)];
+    startPath = [];
+    i = connectionPoint;
+    while ~strcmp(i, 'start')
+        startPath = [startNodes(i).q start(5) start(6); startPath];
+        i = startNodes(i).lastNode;
     end
-    for i = startTreeSize: startTreeSize + goalTreeSize - 1
-        path(i, :) = [goalNodes(goalPath{i - startTreeSize + 1}) goal(5) goal(6)];
+    startPath = [start; startPath];
+    goalPath = [];
+    i = connectionPoint;
+    while ~strcmp(i, 'goal')
+        goalPath = [goalPath; goalNodes(i).q goal(5) goal(6)];
+        i = goalNodes(i).lastNode;
     end
+    goalPath = [goalPath; goal];
+    path = [startPath(1:size(startPath, 1) - 1, :); goalPath]
 end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
