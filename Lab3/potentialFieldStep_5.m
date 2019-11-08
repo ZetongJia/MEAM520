9727349bfc51b0166ecac9e4b37a366e8b9a66dd
Zetong Jia <tina_jia@Zetongs-MacBook-Pro.local>
2019-11-08 01:21:33 -0500

    potential field

--- Lab3/potentialFieldStep_5.m
+++ Lab3/potentialFieldStep_5.m
@@ -24,15 +24,52 @@ function [qNext, isDone] = potentialFieldStep_groupno(qCurr, map, robot)
 
 %%
 
-qNext = zeros(1,6);
-qNext = qCurr;
-qNext(1) = qNext(1)+.01;
-isDone = 0;
+% qNext = zeros(1,6);
+% qNext = qCurr;
+% qNext(1) = qNext(1)+.01;
+% isDone = 0;
+
+zeta = [0,0,0,0,0,0,0.01];
+rConToPar = 30;
+alpha = 0.05;
+rho0 = 200;
+eta = [0,1000,1000,1000,1000,1000,1000];
+tolToGoal = 5;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%                  Algorithm Starts Here             %%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
+[jointPositionsCurr,T0iCurr] = calculateFK_sol(qCurr, robot);
+[jointPositionsGoal,T0iGoal] = calculateFK_sol(map.goal, robot);
+[num, ~] = size(map.obstacles);
+%attractive force for all joints
+Fa = zeta' .* (jointPositionsCurr - jointPositionsGoal);
+%loop through each joint
+for i = 1:7
+    tauSum = [0;0;0;0;0;0];
+    J = calcJacobian_5(qCurr, i, robot);
+    F = [0,0,0,0,0,0];
+    %what is F(4:6) 
+    F(1:3) = Fa(i,:);
+    tau = forceToTorque_groupno(F, J);
+    tauSum = tauSum+tau;
+    for j = 1:num
+        [dist, unit] = distPointToBox(jointPositionsCurr(i,:), map.obstacles(j,:));
+        if (dist > rho0)
+            Fr = [0;0;0;0;0;0];
+        else
+            positionDiff = jointPositionsCurr(i,:) - map.obstacles(j,:);
+            direction = positionDiff/norm(positionDiff);
+            Fr = [0,0,0,0,0,0];
+            FrLinear = eta(i)*((1/dist)-(1/rho0))*(1/dist^2)*(direction);
+            Fr(1:3) = FrLinear;
+        end
+        tau2 = forceToTorque_groupno(Fr, J);
+        tauSum = tauSum+tau2;
+    end 
+    
+    
+end 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%                  Algorithm Ends Here               %%%
